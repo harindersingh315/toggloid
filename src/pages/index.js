bc7c74bb7db5c6997590b3236282e1bbfa5b8d5c
harindersingh315 <79828326+harindersingh315@users.noreply.github.com>
2021-04-02 11:03:49 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,76 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react'
+import { FaTwitter,FaFacebookF,FaInstagram } from 'react-icons/fa'
+import {graphql} from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Newsletter from '../components/newsletter'
+
+
+const bgVideo = require('../dist/mp4/bg.mp4')
+
+const IndexPage = ({data}) => (
   <Layout>
-   <div className="overlay" /></div>
-  <video playsinline=”playsinline” autoplay=”autoplay” muted=”muted” loop=”loop”>
-<source src={bgVideo} type=”video/mp4" />
-</video>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <div className="overlay"></div>
+    <video playsInline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
+      <source src={bgVideo} type="video/mp4" />
+    </video>
 
-  <div className="masthead" />
-    <div className="masthead-bg" /></div>
-    <div className="container h-100" />
-      <div className="row h-100" />
-        <div className="col-12 my-auto" />
-          <div className="masthead-content text-white py-5 py-md-0" />
-            <h1 className="mb-3" />Coming Soon!</h1>
-            <p className="mb-5" />We're working hard to finish the development of this site. Our target launch date is
-              <strong />January 2022</strong>! Sign up for updates using the form below!</p>
-            <div className="input-group input-group-newsletter" />
-              <input type="email" class="form-control" placeholder="Enter email..." aria-label="Enter email..." aria-describedby="submit-button" />
-              <div className="input-group-append" />
-                <button className="btn btn-secondary" type="button" id="submit-button" />Notify Me!</button>
-              </div>
+    <div className="masthead">
+      <div className="masthead-bg"></div>
+      <div className="container h-100">
+        <div className="row h-100">
+          <div className="col-12 my-auto">
+            <div className="masthead-content text-white py-5 py-md-0">
+              <h1 className="mb-3">Coming Soon!</h1>
+              <p className="mb-5">{data.site.siteMetadata.launchpitch} Our target launch date is&nbsp;
+                <strong>{data.site.siteMetadata.launchdate}</strong>! Sign up for updates using the form below!</p>
+                <Newsletter />
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="social-icons">
-    <ul class="list-unstyled text-center mb-0">
-      <li class="list-unstyled-item">
-        <a href="#">
-          <i class="fab fa-twitter"></i>
-        </a>
-      </li>
-      <li class="list-unstyled-item">
-        <a href="#">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-      </li>
-      <li class="list-unstyled-item">
-        <a href="#">
-          <i class="fab fa-instagram"></i>
-        </a>
-      </li>
-    </ul>
-  </div>
+    <div className="social-icons">
+      <ul className="list-unstyled text-center mb-0">
+        <li className="list-unstyled-item">
+          <a href={data.site.siteMetadata.twitterlink} target="_blank">
+            <FaTwitter />
+          </a>
+        </li>
+        <li className="list-unstyled-item">
+        <a href={data.site.siteMetadata.facebookpage} target="_blank">
+            <FaFacebookF />
+          </a>
+        </li>
+        <li className="list-unstyled-item">
+        <a href={data.site.siteMetadata.instagramlink} target="_blank">
+            <FaInstagram />
+          </a>
+        </li>
+      </ul>
+    </div>
+
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        launchdate
+        launchpitch
+        twitterlink
+        facebookpage
+        instagramlink
+      }
+    }
+  }
+`
+
+
