032ecfab9d9065fdcb9b6d3b29e1e6c450642011
harindersingh315 <79828326+harindersingh315@users.noreply.github.com>
2021-04-02 11:05:32 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,29 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import './layout.css'
 
-
-
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            description
+            twitterlink
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+          {children}
+      </>
+    )}
+  />
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
